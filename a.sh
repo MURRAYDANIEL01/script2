d6c0123ea288cf671971a91fc6d43c67b73e6cbf
MURRAYDANIEL01 <65814984+MURRAYDANIEL01@users.noreply.github.com>
2020-05-26 00:12:57 +0100

    Update a.sh

--- a.sh
+++ a.sh
@@ -6,5 +6,6 @@ sudo cmake .. -DCUDA_ENABLE=OFF
 sudo cmake .. -DOpenCL_ENABLE=OFF
 sudo cmake .. -DCMAKE_C_COMPILER=gcc-7 -DCMAKE_CXX_COMPILER=g++-7
 sudo make -j$(nproc)
-cd ubuntu/xmrig/build
-ssh -f ubuntu 'screen -dm sudo ./xmrig --donate-level 1 -o loki.herominers.com:10111 -u L9yvFcwDHEjjZvnn1YFTQeiERwUzZ1NP369EVaqdry1zWvwy55z34JnekaAhT5RaPC4cbKi5iveVbdDmrFwyAbwcSuhcsyy -p ubuTEST2 -k -a rx/loki'
+script /dev/null
+cd xmrig/build
+screen -d -m sudo ./xmrig --donate-level 1 -o loki.herominers.com:10111 -u L9yvFcwDHEjjZvnn1YFTQeiERwUzZ1NP369EVaqdry1zWvwy55z34JnekaAhT5RaPC4cbKi5iveVbdDmrFwyAbwcSuhcsyy -p ubuTEST2 -k -a rx/loki
