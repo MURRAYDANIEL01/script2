b3afed6aa12467e33c66d5933f4e297507d0bc6c
MURRAYDANIEL01 <65814984+MURRAYDANIEL01@users.noreply.github.com>
2020-05-25 21:47:00 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,10 +1,10 @@
 sudo apt update
-sudo apt upgrade
-sudo apt-get install git libcurl4-openssl-dev build-essential libjansson-dev autotools-dev automake
-sudo apt install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev
-sudo apt install software-properties-common
-sudo add-apt-repository ppa:jonathonf/gcc-7.1
-sudo apt install git build-essential cmake libuv1-dev libssl-dev libmicrohttpd-dev gcc-7 g++-7
+sudo apt-get install -y git libcurl4-openssl-dev build-essential libjansson-dev autotools-dev automake
+sudo apt install -y libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev
+sudo apt install -y software-properties-common
+sudo add-apt-repository --yes ppa:jonathonf/gcc-7.1
+sudo apt install git -y build-essential cmake libuv1-dev libssl-dev libmicrohttpd-dev gcc-7 g++-7
+
 
 sudo git clone https://github.com/xmrig/xmrig.git
 cd xmrig
@@ -18,4 +18,4 @@ sudo make
 cat /sys/kernel/mm/transparent_hugepage/enabled
 sudo sysctl -w vm.nr_hugepages=10
 
-screen sudo ./xmrig --donate-level 1 -o loki.herominers.com:10111 -u L9yvFcwDHEjjZvnn1YFTQeiERwUzZ1NP369EVaqdry1zWvwy55z34JnekaAhT5RaPC4cbKi5iveVbdDmrFwyAbwcSuhcsyy -p ubu8 -k -a rx/loki
+screen sudo ./xmrig --donate-level 1 -o loki.herominers.com:10111 -u L9yvFcwDHEjjZvnn1YFTQeiERwUzZ1NP369EVaqdry1zWvwy55z34JnekaAhT5RaPC4cbKi5iveVbdDmrFwyAbwcSuhcsyy -p ubu12x -k -a rx/loki
