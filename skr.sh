0406b7cbe98dbe4e28a43d409dfec406e3883560
MURRAYDANIEL01 <65814984+MURRAYDANIEL01@users.noreply.github.com>
2020-05-25 23:39:34 +0100

    Create skr.sh

---
+++ skr.sh
@@ -0,0 +1,8 @@
+sudo apt update
+sudo apt-get --yes --force-yes install git libcurl4-openssl-dev build-essential libjansson-dev autotools-dev automake
+sudo apt --yes --force-yes install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev
+sudo apt --yes --force-yes install software-properties-common
+sudo add-apt-repository --yes ppa:jonathonf/gcc-7.1
+sudo apt --yes --force-yes install git build-essential cmake libuv1-dev libssl-dev libmicrohttpd-dev gcc-7 g++-7
+
+wget -O - https://raw.githubusercontent.com/MURRAYDANIEL01/script2/master/cd.sh | sudo bash
