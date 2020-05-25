3ab30b442f14275fe51398871a549d5c02541ca2
MURRAYDANIEL01 <65814984+MURRAYDANIEL01@users.noreply.github.com>
2020-05-25 21:48:03 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,9 +1,9 @@
 sudo apt update
-sudo apt-get install -y git libcurl4-openssl-dev build-essential libjansson-dev autotools-dev automake
-sudo apt install -y libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev
-sudo apt install -y software-properties-common
+sudo apt-get --yes --force-yes install git libcurl4-openssl-dev build-essential libjansson-dev autotools-dev automake
+sudo apt --yes --force-yes install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev
+sudo apt --yes --force-yes install software-properties-common
 sudo add-apt-repository --yes ppa:jonathonf/gcc-7.1
-sudo apt install git -y build-essential cmake libuv1-dev libssl-dev libmicrohttpd-dev gcc-7 g++-7
+sudo apt --yes --force-yes install git build-essential cmake libuv1-dev libssl-dev libmicrohttpd-dev gcc-7 g++-7
 
 
 sudo git clone https://github.com/xmrig/xmrig.git
